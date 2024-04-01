fa760ea81980db22222f9a6873a9ce036d9502d4
Ankita <deeparajput5348@gmail.com>
2024-04-01 16:48:46 +0530

    initi

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -14,10 +14,6 @@ function HomeCarousel() {
           height="500px"
           alt="First slide"
         />
-        {/* <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -27,11 +23,6 @@ function HomeCarousel() {
           height="500px"
           alt="Second slide"
         />
-
-        {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -41,13 +32,6 @@ function HomeCarousel() {
           height="500px"
           alt="Third slide"
         />
-
-        {/* <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption> */}
       </Carousel.Item>
     </Carousel>
   );
